0379fefac397c7679ea988702a67f414b5204ced
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2024-04-03 18:38:04 +0200

    create

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import css from "./App.module.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className={css.header}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
